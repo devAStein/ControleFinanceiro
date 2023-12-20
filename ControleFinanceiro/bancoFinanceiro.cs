dbc5b06878798bed9dcad817021d4dafede24503
Armando Stein <armandostein@hotmail.com>
2023-12-20 15:38:37 -0300

    Inclusão da entrada no banco de dados financeiro.

---
+++ ControleFinanceiro/bancoFinanceiro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace ControleFinanceiro
+{
+    internal class bancoFinanceiro
+    {
+        public string bancoF = "SERVER=localhost; DATABASE=sqlFinanceiro; UID=root; PWD=; PORT=";
+
+        public MySqlConnection acessBanco = null;
+
+        //abrir banco de dados financeiro
+        public void abrirBancoFinanceiro()
+        {
+            try
+            {
+                acessBanco = new MySqlConnection(bancoF);
+                acessBanco.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro no servidor: ==>" + ex.Message);
+            }
+        }
+
+        //fechar banco de dados financeiro
+
+        public void fecharBancoFinanceiro()
+        {
+            try
+            {
+                acessBanco = new MySqlConnection(bancoF);
+                acessBanco.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro no servidor: ==> " + ex.Message);
+            }
+        }
+    }
+}
--- ControleFinanceiro/inicial.cs
+++ ControleFinanceiro/inicial.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,41 @@ namespace ControleFinanceiro
 {
     public partial class inicial : Form
     {
+        bancoFinanceiro acessBanco = new bancoFinanceiro();
+        string sql;
+        MySqlCommand cmd;
+
         public inicial()
         {
             InitializeComponent();
+        }
+
+        private void btnEntrada_Click(object sender, EventArgs e)
+        {
+            if(txtEntrData.Text.ToString().Trim() == "" || txtEntrIdentificador.Text.ToString().Trim() == "" || txtEntrValor.Text.ToString().Trim() == "")
+            {
+                txtEntrData.Text = "";
+                txtEntrIdentificador.Text = "";
+                txtEntrValor.Text = "";
+                msgErroEntr.Visible = true;
+            }
+            else
+            {
+                acessBanco.abrirBancoFinanceiro();
+                sql = "INSERT INTO dados (dataEntrada, idenEntrada, valorEntrada) VALUES(@dataEntrada, @idenEntrada, @valorEntrada)";
+                cmd = new MySqlCommand(sql, acessBanco.acessBanco);
+                cmd.Parameters.AddWithValue("@dataEntrada", txtEntrData.Text);
+                cmd.Parameters.AddWithValue("@idenEntrada", txtEntrIdentificador.Text);
+                cmd.Parameters.AddWithValue("@valorEntrada", txtEntrValor.Text);
+                cmd.ExecuteNonQuery();
+                acessBanco.fecharBancoFinanceiro();
+                txtEntrData.Text = "";
+                txtEntrIdentificador.Text = "";
+                txtEntrValor.Text = "";
+                msgErroEntr.Visible = false;
+                msgSuceEntr.Visible = true;
+            }
+
         }
     }
 }
